e09fa1aa6c6d37d4fd1c6822ee8aaf4b956961c6
nina444-dot <ninadominguez1707@gmail.com>
2026-05-03 20:47:27 +0200

    Environnement Tailwind fonctionnel avec output.css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,9 @@
 function App() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-blue-500">
-      <h1 className="text-white text-5xl font-bold p-10 bg-black rounded-2xl shadow-2xl">
-        Tailwind est activé ! 
-      </h1>
-    </div>
-  )
+   <div className="bg-red-500 h-screen flex justify-center items-center">
+  <h1 className="text-white text-4xl">TEST ROUGE</h1>
+</div>
+  );
 }
 
-export default App
+export default App;
