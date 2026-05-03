1f27aeb9ff39a1460d5bc680d6edffedc135db5e
nina444-dot <ninadominguez1707@gmail.com>
2026-05-03 02:40:22 +0200

    Mise en place de la connexion à la BDD et du modèle des séances

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,20 @@
-const mysql = require('mysql2');
-require('dotenv').config();
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+import mysql from 'mysql2/promise';
+import 'dotenv/config';
+
+export const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
 });
 
-module.exports = db
+// Test de connexion
+pool.getConnection()
+    .then((conn) => {
+        console.log("Connexion à la BDD réussie !");
+        conn.release();
+    })
+    .catch((err) => {
+        console.error(" Erreur de connexion :", err.message);
+    });
---
+++ backend/model/seance.js
@@ -0,0 +1,19 @@
+import { pool } from "../config/db.js";
+
+
+export const findAllSeances = async () => {
+    const [rows] = await pool.query("SELECT * FROM seances_standard");
+    return rows;
+};
+
+
+export const findAllFormules = async () => {
+    const [rows] = await pool.query("SELECT * FROM formules_standard");
+    return rows;
+};
+
+// Cherche une séance précise par son ID
+export const findSeanceById = async (id) => {
+    const [rows] = await pool.query("SELECT * FROM seances_standard WHERE id = ?", [id]);
+    return rows[0];
+};
