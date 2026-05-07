87ebd91af0ba170ec2aebc1a726a9c4d934bdd2b
nina444-dot <ninadominguez1707@gmail.com>
2026-05-07 23:57:29 +0200

    Création du fichier Footer et insertion des logos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,16 @@
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="bg-hazel-light min-h-screen font-cormorant">
+    <div className="bg-hazel-light min-h-screen font-cormorant flex flex-col">
       <Navbar transparent={false}/>
       
+      <main className="flex-grow">
+        
+      </main>
+
+      <Footer />
     </div>
   );
 }
